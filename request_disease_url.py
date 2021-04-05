321aee57f02e6f0130da1878ecef15d6833218e5
wangtao2001 <wangtao.cpu@gmail.com>
2021-04-05 21:37:01 +0800

    finish get disease_url and no repetition

--- request_disease_url.py
+++ request_disease_url.py
@@ -5,7 +5,7 @@
 
 import requests
 import json
-import bs4
+import time
 from bs4 import BeautifulSoup
 
 li = []  # 去重列表
@@ -17,18 +17,18 @@ with open(r".\data\department.json", "r", encoding="utf-8") as department_f:
     department_dict = json.load(department_f, encoding="utf-8")
 url_list = [start_url + de for key in department_dict.keys() for de in department_dict[key]]
 
-
 disease_url_f = open(r".\data\disease_url.txt", "a", encoding="utf-8")  # 追加写
 
 disease_url_dict = dict()
-for url in url_list[:10]:  # url_list长度为41 索引0:41
+for url in url_list:  # url_list长度为41 索引0:41
+
     r1 = requests.get(url, headers=headers, timeout=15)
     soup1 = BeautifulSoup(r1.content, "html.parser")
 
     ul = soup1.find_all("ul", attrs={"class": "result_item_dots"})[0]
     page_num = int(ul.find_all("a")[-2].string)  # 页数
 
-    for i in range(1, page_num+1):  # 每个科室下的每一页
+    for i in range(1, page_num + 1):  # 每个科室下的每一页
         r1 = requests.get(url + f'_p{i}', headers=headers, timeout=15)
         soup2 = BeautifulSoup(r1.content, "html.parser")
 
@@ -48,5 +48,5 @@ for url in url_list[:10]:  # url_list长度为41 索引0:41
                     disease_url_f.write(str(disease_url_dict))  # txt文件将会包含很多字典，每一个字典仅包含一个键值对 疾病：url
                     disease_url_f.write("\n")
                 disease_url_dict = dict()
-                
+
 disease_url_f.close()
