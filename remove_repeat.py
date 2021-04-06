8ed0823126ce6316bc0281be5c0c80d52dba18cb
wangtao2001 <wangtao.cpu@gmail.com>
2021-04-06 20:40:47 +0800

    remove repeat

--- remove_repeat.py
+++ remove_repeat.py
@@ -3,3 +3,22 @@
 # @author:  wangtao
 # @data: 21/04/06, Mon
 
+class RR:
+    def __init__(self, file, name):
+        li = [i.strip() for i in file.readlines()]  # 每个元素都是字典
+        print(f"去重前：{len(li)}")
+        li = list(set(li))
+        print(f"去重后：{len(li)}")
+        with open(rf".\data\{name}.txt", "w", encoding='utf-8') as f:
+            for i in li:
+                f.write(i)
+                f.write("\n")
+
+
+if __name__ == "__main__":
+    with open(r".\data\medical_url_repeat.txt", "r", encoding='utf-8') as file:
+        RR(file, "medical_url")
+    with open(r".\data\check_url_repeat.txt", "r", encoding='utf-8') as file:
+        RR(file, "check_url")
+    with open(r".\data\symptom_url_repeat.txt", "r", encoding='utf-8') as file:
+        RR(file, "symptom_url")
